f4bc8ac2eeb76161ddf2d52224d7190ad5beaea7
Luke Francl <lfrancl@swiftype.com>
2013-02-27 15:52:24 -0800

    add channels DocumentType

--- create_engine.sh
+++ create_engine.sh
@@ -3,6 +3,7 @@
 API_HOST='https://api.swiftype.com'
 ENGINE_NAME='swiftype-api-example'
 VIDEOS='./data/videos/*.json'
+CHANNELS='./data/channels/*.json'
 
 if [ $# -eq 0 ]; then
     echo "Please provide your engine key as a command line argument."
@@ -22,11 +23,19 @@ curl -s -w "Engine create: HTTP %{http_code}\n" \
     -o /dev/null
 
 # Create the videos DocumentType
-curl -s -w "DocumentType create: HTTP %{http_code}\n" \
+curl -s -w "videos DocumentType create: HTTP %{http_code}\n" \
     -X POST "$API_HOST/api/v1/engines/$ENGINE_NAME/document_types.json?auth_token=$1" \
     -H 'Content-Type: application/json' \
     -d '{"document_type": {"name": "videos"}}' \
     -o /dev/null
+
+# Create the channels DocumentType
+curl -s -w "channels DocumentType create: HTTP %{http_code}\n" \
+    -X POST "$API_HOST/api/v1/engines/$ENGINE_NAME/document_types.json?auth_token=$1" \
+    -H 'Content-Type: application/json' \
+    -d '{"document_type": {"name": "channels"}}' \
+    -o /dev/null
+
 
 # Create the Documents in the videos DocumentType
 for f in $VIDEOS
@@ -34,6 +43,15 @@ do
     echo "Processing $f"
     curl -s -w "Document create for $f: HTTP %{http_code}\n" \
         -X POST "$API_HOST/api/v1/engines/$ENGINE_NAME/document_types/videos/documents/create_or_update.json?auth_token=$1" \
+        -H 'Content-Type: application/json' \
+        --data @$f
+done
+
+for f in $CHANNELS
+do
+    echo "Processing $f"
+    curl -s -w "Document create for $f: HTTP %{http_code}\n" \
+        -X POST "$API_HOST/api/v1/engines/$ENGINE_NAME/document_types/channels/documents/create_or_update.json?auth_token=$1" \
         -H 'Content-Type: application/json' \
         --data @$f
 done
