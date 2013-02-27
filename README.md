fd84828c4206de72b6e985649d14510393a9bb2f
Luke Francl <lfrancl@swiftype.com>
2013-02-26 18:19:09 -0800

    initial commit of a script that can create an API-based engine and add some documents to it

---
+++ README.md
@@ -0,0 +1,19 @@
+# Swiftype API Example
+
+This repository contains data and scripts for creating a Swiftype search engine you can experiment with.
+
+## Public example engine
+
+You can find a read-only version of this engine on Swiftype with the engine key `swiftype-api-example`. Here is an example query:
+
+https://api.swiftype.com/api/v1/public/engines/search.json?q=glass&engine_key=swiftype-api-example
+
+## Creating your own engine
+
+To create your own engine, first [sign up for a Swiftype account](https://swiftype.com), then run the `create_engine.sh` command with your API key:
+
+`./create_engine.sh YOUR_API_KEY`
+
+This should work on Unix-based operating systems (patches for a Windows script are welcome).
+
+Note: `curl` is required to run the `create_engine.sh` script.
---
+++ create_engine.sh
@@ -0,0 +1,39 @@
+#! /bin/sh
+
+API_HOST='https://api.swiftype.com'
+ENGINE_NAME='swiftype-api-example'
+VIDEOS='./data/videos/*.json'
+
+if [ $# -eq 0 ]; then
+    echo "Please provide your engine key as a command line argument."
+    exit 1
+fi
+
+if [ $# -gt 1 ]; then
+    echo "Too many command line arguments."
+    exit 1
+fi
+
+# Create the Engine
+curl -s -w "Engine create: HTTP %{http_code}\n" \
+    -X POST "$API_HOST/api/v1/engines.json?auth_token=$1" \
+    -H 'Content-Type: application/json' \
+    -d "{\"engine\":{\"name\": \"$ENGINE_NAME\"}}" \
+    -o /dev/null
+
+# Create the videos DocumentType
+curl -s -w "DocumentType create: HTTP %{http_code}\n" \
+    -X POST "$API_HOST/api/v1/engines/$ENGINE_NAME/document_types.json?auth_token=$1" \
+    -H 'Content-Type: application/json' \
+    -d '{"document_type": {"name": "videos"}}' \
+    -o /dev/null
+
+# Create the Documents in the videos DocumentType
+for f in $VIDEOS
+do
+    echo "Processing $f"
+    curl -s -w "Document create for $f: HTTP %{http_code}\n" \
+        -X POST "$API_HOST/api/v1/engines/$ENGINE_NAME/document_types/videos/documents/create_or_update.json?auth_token=$1" \
+        -H 'Content-Type: application/json' \
+        --data @$f
+done
